6f4acfd074e42027b8c16217032b637e51a842f6
emirakbas <makbas1@ogr.iu.edu.tr>
2024-09-30 17:56:15 +0300

    Kullanıcıdan input verisinin çekilmesi tamamlandı.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('#createTable').addEventListener('click', function() {
+    let tableName = document.querySelector('#tableName').value;
+    let userName = document.querySelector('#userName').value;
+    let surName = document.querySelector('#surName').value;
+    let userNumber = document.querySelector('#userNumber').value;
+    console.log(tableName + ' ' + userName + ' ' + surName + ' ' + userNumber);
+
+    let createTable = document.createElement('???'); //Bunu tamamla.
+});

ec2b867d37c31c8e36fa4fcbece581152755d076
emirakbas <makbas1@ogr.iu.edu.tr>
2024-10-01 16:43:01 +0300

    Tablo ekleme kodlarının bir kısmı yazıldı.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var rowCounter = 0;
+var columnCounter = 0;
+var tableNumber = 0;
+
 document.querySelector('#createTable').addEventListener('click', function() {
     let tableName = document.querySelector('#tableName').value;
     let userName = document.querySelector('#userName').value;
@@ -5,5 +9,76 @@ document.querySelector('#createTable').addEventListener('click', function() {
     let userNumber = document.querySelector('#userNumber').value;
     console.log(tableName + ' ' + userName + ' ' + surName + ' ' + userNumber);
 
-    let createTable = document.createElement('???'); //Bunu tamamla.
+    // TABLO OLUŞTURMA KODLARI:
+        let tableAdress = document.querySelector('#tablesContainer'); //Oluşturacağımız konum.
+        let createTable = document.createElement('div');
+
+        tableAdress.appendChild(createTable);
+        createTable.className = 'table';
+        createTable.id = `table${tableNumber}`;
+    // TABLO OLUŞTURMA KODLARI BİTİŞ.
+
+    // TABLO HEADER OLUŞTURMA KODLARI:
+        let tableHeaderAdress = document.querySelector(`#table${tableNumber}`);
+        let createTableHeader = document.createElement('div');
+
+        tableHeaderAdress.appendChild(createTableHeader);
+        createTableHeader.className = 'tableButtons';
+        createTableHeader.id = `tableHeader${tableNumber}`;
+    // TABLO HEADER OLUŞTURMA KODLARI BİTİŞ.
+
+    // TABLO BAŞLIK OLUŞTURMA KODLARI:
+        let tableButUpAdress = document.querySelector(`#tableHeader${tableNumber}`);
+        let createTableButUp = document.createElement('div');
+
+        tableButUpAdress.appendChild(createTableButUp);
+        createTableButUp.className = 'tableButUp';
+        createTableButUp.innerHTML = 'Tablo Adı'; // !!! TABLO ADI BURADA YAZAR !!!
+
+        let tableButDownAdress = document.querySelector(`#tableHeader${tableNumber}`);
+        let createTableButDown = document.createElement('div');
+
+        tableButDownAdress.appendChild(createTableButDown);
+        createTableButDown.className = 'tableButDown';
+        createTableButDown.id = `tableButDown${tableNumber}`;
+
+        let tableButtonsAdress = document.querySelector(`#tableButDown${tableNumber}`);
+        let createTableButtons = document.createElement('button');
+
+        tableButtonsAdress.appendChild(createTableButtons);
+        createTableButtons.className = 'add';
+        createTableButtons.id = `add${tableNumber}`;
+        createTableButtons.innerHTML = 'Ekle';
+
+
+    // TABLO BAŞLIK OLUŞTURMA KODLARI:
+
+    // SATIR OLUŞTURMA KODLARI:
+        let tableInsideAdress = document.querySelector(`#table${tableNumber}`);
+        let createRow = document.createElement('div'); //Tabloya satır oluştur.
+        tableInsideAdress.appendChild(createRow); //Oluşturulanları HTML'e ekle.
+        createRow.className = 'tableRow'; //Oluşturulanlara class ismi ver.
+        createRow.id = `tableRow${rowCounter}`; //Kolon ID'sini her defasında 1 artacak şekilde ayarla.
+    // SATIR OLUŞTURMA KODLARI BİTİŞ.
+
+    // SÜTUN OLUŞTURMA KODLARI:
+        let rowAdress = document.querySelector(`#tableRow${rowCounter}`);
+        
+        for (i = 1; i <= 4; i++) {
+        let createColumn = document.createElement('div');
+    
+        rowAdress.appendChild(createColumn);
+        createColumn.className = `tableColumn`;
+        createColumn.id = `tableColumn${columnCounter}`;
+
+        document.querySelector(`#tableColumn${columnCounter}`).innerHTML = 'Örnek';
+
+        columnCounter++;
+        };
+    // SÜTUN OLUŞTURMA KODLARI BİTİŞ.
+
+    tableNumber++;
+    rowCounter++; //Her tıklamada 1 arttır.
+    console.log(`Kolon: ${columnCounter} \nSatır: ${rowCounter}`);
+
 });

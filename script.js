7a9fbcda9509b9917653b5d081ed209e88cd6cfc
emirakbas <makbas1@ogr.iu.edu.tr>
2024-10-02 14:07:10 +0300

    Tablo butonları çalışır hale getirildi. Tablo içeriğini düzenleme seçenekleri eklendi.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 var rowCounter = 0;
 var columnCounter = 0;
 var tableNumber = 0;
+var focusInput = document.querySelector('#tableName');
+focusInput.focus();
 
 document.querySelector('#createTable').addEventListener('click', function() {
     let tableName = document.querySelector('#tableName').value;
     let userName = document.querySelector('#userName').value;
     let surName = document.querySelector('#surName').value;
     let userNumber = document.querySelector('#userNumber').value;
+    let tableTitle = document.querySelector('#tableTitle').value;
     console.log(tableName + ' ' + userName + ' ' + surName + ' ' + userNumber);
+
+        // REGEX KONTROLÜ:
+        function regexControl(str) {
+            // Düzenli ifade ile sayıları bul
+                const regex = /\d+/g;           // \d+ = 1 veya daha fazla sayı bul demek. g = global, yani tüm sayıları bul.  
+                const numbers = str.match(regex);
+            // Eğer sayı yoksa boş dizi döner, varsa sayıları döner
+                return numbers ? numbers.map(Number) : [];
+        }
+        // REGEX KONTROLÜ BİTİŞ.
 
     // TABLO OLUŞTURMA KODLARI:
         let tableAdress = document.querySelector('#tablesContainer'); //Oluşturacağımız konum.
@@ -30,14 +43,19 @@ document.querySelector('#createTable').addEventListener('click', function() {
     // TABLO HEADER İÇİ KODLARI:
         let tableButUpAdress = document.querySelector(`#tableHeader${tableNumber}`);
         let createTableButUp = document.createElement('div');
-
+        //
         tableButUpAdress.appendChild(createTableButUp);
         createTableButUp.className = 'tableButUp';
-        createTableButUp.innerHTML = 'Tablo Adı'; // !!! TABLO ADI BURADA YAZAR !!!
 
+        if (tableName == '') {
+            createTableButUp.innerHTML = `Tablo ${tableNumber}`;
+        } else {
+            createTableButUp.innerHTML = tableName;
+        }
+        //  //
         let tableButDownAdress = document.querySelector(`#tableHeader${tableNumber}`);
         let createTableButDown = document.createElement('div');
-
+        // //
         tableButDownAdress.appendChild(createTableButDown);
         createTableButDown.className = 'tableButDown';
         createTableButDown.id = `tableButDown${tableNumber}`;
@@ -67,6 +85,81 @@ document.querySelector('#createTable').addEventListener('click', function() {
         createTableButtons4.className = 'delete';
         createTableButtons4.id = `delete${tableNumber}`;
         createTableButtons4.innerHTML = 'Tabloyu Sil';
+
+        // TABLO BUTONLARI İÇİN EVENTLER:
+
+            // EKLE BUTONU İÇİN EVENT:
+            createTableButtons.addEventListener('click', function() {
+                console.log('Ekle butonuna tıklandı.');
+
+                addControl = regexControl(createTableButtons.id);
+                console.log('Ekle ID: ' + addControl);
+                
+                tableIDControl = regexControl(createTable.id);
+                console.log('Tablo ID: ' + tableIDControl);
+
+                addButStrToNum1 = parseInt(addControl);
+                addButStrToNum2 = parseInt(tableIDControl);
+
+                if (addButStrToNum1 == addButStrToNum2) {
+                    console.log('Ekleme işlemi başarılı.');
+                } else {
+                    console.log('Ekle işlemi başarısız.');
+                }
+            });
+            
+            // DÜZENLE BUTONU İÇİN EVENT:
+            createTableButtons2.addEventListener('click', function() {
+                console.log('Düzenle butonuna tıklandı.');
+
+                editControl = regexControl(createTableButtons2.id);
+                tableIDControl = regexControl(createTable.id);
+
+                editButStrToNum1 = parseInt(editControl);
+                editButStrToNum2 = parseInt(tableIDControl);
+
+                if (editButStrToNum1 == editButStrToNum2) {
+                    console.log('Düzenleme işlemi başarılı.');
+                } else {
+                    console.log('Düzenleme işlemi başarısız.');
+                }
+            });
+            
+            // TABLOYU TEMİZLE BUTONU İÇİN EVENT:
+            createTableButtons3.addEventListener('click', function() {
+                console.log('Tabloyu Temizle butonuna tıklandı.');
+
+                clearControl = regexControl(createTableButtons3.id);
+                tableIDControl = regexControl(createTable.id);
+
+                clearButStrToNum1 = parseInt(clearControl);
+                clearButStrToNum2 = parseInt(tableIDControl);
+
+                if (clearButStrToNum1 == clearButStrToNum2) {
+                    console.log('Temizleme işlemi başarılı.');
+                } else {
+                    console.log('Temizleme işlemi başarısız.');
+                }
+            });
+            
+            // TABLOYU SİL BUTONU İÇİN EVENT:
+            createTableButtons4.addEventListener('click', function() {
+                console.log('Tabloyu Sil butonuna tıklandı.');
+
+                deleteControl = regexControl(createTableButtons4.id);
+                tableIDControl = regexControl(createTable.id);
+
+                deleteButStrToNum1 = parseInt(deleteControl);
+                deleteButStrToNum2 = parseInt(tableIDControl);
+
+                if (deleteButStrToNum1 == deleteButStrToNum2) {
+                    console.log('Silme işlemi başarılı.');
+                } else {
+                    console.log('Silme işlemi başarısız.');
+                }
+            });
+        // TABLO BUTONLARI İÇİN EVENTLER BİTİŞ.
+
     // TABLO HEADER İÇİ OLUŞTURMA KODLARI BİTİŞ.
 
     // SATIR OLUŞTURMA KODLARI:
@@ -80,7 +173,7 @@ document.querySelector('#createTable').addEventListener('click', function() {
     // SÜTUN OLUŞTURMA KODLARI:
         let rowAdress = document.querySelector(`#tableRow${rowCounter}`);
         
-        for (i = 1; i <= 4; i++) {
+        for (i = 1; i <= 3; i++) {
         let createColumn = document.createElement('div');
 
         rowAdress.appendChild(createColumn);
@@ -89,23 +182,41 @@ document.querySelector('#createTable').addEventListener('click', function() {
 
         // SÜTUN BAŞLIKLARI İÇİN DÖNGÜ:
             if (i == 1) {
-                createColumn.innerHTML = 'İsim';
+                createColumn.innerHTML = userName;
             } else if (i == 2) {
-                createColumn.innerHTML = 'Soyisim';
+                createColumn.innerHTML = surName;
             }
-            else if (i == 3) {
-                createColumn.innerHTML = 'Numara';
-            }
-            else if (i == 4) {
-                createColumn.innerHTML = 'İşlem';
+            else {
+                createColumn.innerHTML = userNumber;
             };
+
+            if (userName == '' || surName == '' || userNumber == '') {
+                createColumn.innerHTML = '-';
+            }
         // SÜTUN BAŞLIKLARI İÇİN DÖNGÜ BİTİŞ.
-
-        // SÜTUN İÇİNDEKİ İÇERİKLERİ OLUŞTURMA:
-
 
         columnCounter++;
         };
+
+        let tableEdit = document.createElement('div');
+        rowAdress.appendChild(tableEdit);
+        tableEdit.className = 'tableEdit';
+        tableEdit.id = `tableEdit${columnCounter}`;
+
+        let tableEditButton = document.createElement('button');
+        tableEdit.appendChild(tableEditButton);
+        tableEditButton.className = 'edit';
+        tableEditButton.id = `edit${columnCounter}`;
+        tableEditButton.innerHTML = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 117.74 122.88" style="enable-background:new 0 0 117.74 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M94.62,2c-1.46-1.36-3.14-2.09-5.02-1.99c-1.88,0-3.56,0.73-4.92,2.2L73.59,13.72l31.07,30.03l11.19-11.72 c1.36-1.36,1.88-3.14,1.88-5.02s-0.73-3.66-2.09-4.92L94.62,2L94.62,2L94.62,2z M41.44,109.58c-4.08,1.36-8.26,2.62-12.35,3.98 c-4.08,1.36-8.16,2.72-12.35,4.08c-9.73,3.14-15.07,4.92-16.22,5.23c-1.15,0.31-0.42-4.18,1.99-13.6l7.74-29.61l0.64-0.66 l30.56,30.56L41.44,109.58L41.44,109.58L41.44,109.58z M22.2,67.25l42.99-44.82l31.07,29.92L52.75,97.8L22.2,67.25L22.2,67.25z"/></g></svg>';
+
+        let tableDeleteButton = document.createElement('button');
+        tableEdit.appendChild(tableDeleteButton);
+        tableDeleteButton.className = 'delete';
+        tableDeleteButton.id = `delete${columnCounter}`;
+        tableDeleteButton.innerHTML = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="109.484px" height="122.88px" viewBox="0 0 109.484 122.88" enable-background="new 0 0 109.484 122.88" xml:space="preserve"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M2.347,9.633h38.297V3.76c0-2.068,1.689-3.76,3.76-3.76h21.144 c2.07,0,3.76,1.691,3.76,3.76v5.874h37.83c1.293,0,2.347,1.057,2.347,2.349v11.514H0V11.982C0,10.69,1.055,9.633,2.347,9.633 L2.347,9.633z M8.69,29.605h92.921c1.937,0,3.696,1.599,3.521,3.524l-7.864,86.229c-0.174,1.926-1.59,3.521-3.523,3.521h-77.3 c-1.934,0-3.352-1.592-3.524-3.521L5.166,33.129C4.994,31.197,6.751,29.605,8.69,29.605L8.69,29.605z M69.077,42.998h9.866v65.314 h-9.866V42.998L69.077,42.998z M30.072,42.998h9.867v65.314h-9.867V42.998L30.072,42.998z M49.572,42.998h9.869v65.314h-9.869 V42.998L49.572,42.998z"/></g></svg>';
+
+
+
     // SÜTUN OLUŞTURMA KODLARI BİTİŞ.
 
     tableNumber++;

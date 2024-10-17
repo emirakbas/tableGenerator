39a144fcdffc276f03c1a4aa1b5e87e4563e6c00
emirakbas <makbas1@ogr.iu.edu.tr>
2024-10-17 17:25:20 +0300

    Hücre adresleme başarıyla tamamlandı.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var columnCounter = 0;
+var columnCounter = -1;
 var tableNumber = 0;
 var focusInput = document.querySelector('#tableName');
 focusInput.focus();
@@ -135,18 +135,45 @@ document.querySelector('#createTable').addEventListener('click', function() {
                 tableButtonID = parseInt(tableIDControl);
 
                 if (addButtonID == tableButtonID) {
+                    // Satır ID'si için regex kontrolü:
                     console.log('Ekleme işlemi başarılı.');
                     let tableInsideRowAdress = document.querySelector(`#tableInside${addButtonID}`);
                     let createTableInsideRow = document.createElement('div');
-                    tableInsideRowAdress.appendChild(createTableInsideRow);
+                    tableInsideRowAdress.appendChild(createTableInsideRow); 
                     createTableInsideRow.className = 'tableRow';
-/*                     createTableInsideRow.id = `tableRow${tableRowCounter}`;
-                    console.log('Satır ID: ' + tableRowCounter); Todo: Burada kronik hatalar mevcut.  */
+                    let childCount = tableInsideRowAdress.childElementCount; // Satır sayısını alır.
+                    console.log('Satır Sayısı: ' + childCount);
+                    createTableInsideRow.id = `tableRow_${addButtonID}_${childCount}`;
+
+                    for (let i = 1; i <= 3; i++) {
+                        let createColumn = document.createElement('div');
+                        createTableInsideRow.appendChild(createColumn);
+                        createColumn.className = 'tableColumn';
+                        createColumn.id = `tableInside${addButtonID}_Row${childCount}_Col${i}`;
+                        console.log(createColumn.id);
+                    };
+
+                    if (userName == '') {
+                        document.querySelector(`#tableInside${addButtonID}_Row${childCount}_Col1`).innerHTML = '-';
+                    }
+                    if (surName == '') {
+                        document.querySelector(`#tableInside${addButtonID}_Row${childCount}_Col2`).innerHTML = '-';
+                    }
+                    if (userNumber == '') {
+                        document.querySelector(`#tableInside${addButtonID}_Row${childCount}_Col3`).innerHTML = '-';
+                    }
+
+                    let rowEditButtonsAdress = document.querySelector(`#tableRow_${addButtonID}_${childCount}`);
+                    let createRowEditButtons = document.createElement('div');
+                    rowEditButtonsAdress.appendChild(createRowEditButtons);
+                    createRowEditButtons.className = 'tableEdit';
+                    createRowEditButtons.id = `tableEdit${childCount}`;
+
+                    
                 } else {
                     console.log('Ekle işlemi başarısız.');
                 }
             });
-            
             // DÜZENLE BUTONU İÇİN EVENT:
             createTableButtons2.addEventListener('click', function() {
                 console.log('Düzenle butonuna tıklandı.');
@@ -215,11 +242,11 @@ document.querySelector('#createTable').addEventListener('click', function() {
         let createRow = document.createElement('div'); //Tabloya satır oluştur.
         tableRowAdress.appendChild(createRow); //Oluşturulanları HTML'e ekle.
         createRow.className = 'tableRow'; //Oluşturulanlara class ismi ver.
-        createRow.id = `tableRow${tableNumber}`; //Kolon ID'sini her defasında 1 artacak şekilde ayarla.
+        createRow.id = `tableRow_${tableNumber}_1`; //Kolon ID'sini her defasında 1 artacak şekilde ayarladık. Son id numarasının üstüne 1 eklediği için 1 ile başlattık.
     // SATIR OLUŞTURMA KODLARI BİTİŞ.
 
     // SÜTUN OLUŞTURMA KODLARI:
-        let rowAdress = document.querySelector(`#tableRow${tableNumber}`);
+        let rowAdress = document.querySelector(`#tableRow_${tableNumber}_1`);
         
         for (let i = 1; i <= 3; i++) {
         let createColumn = document.createElement('div');
@@ -237,8 +264,8 @@ document.querySelector('#createTable').addEventListener('click', function() {
         ColumnIDControl = regexControl(createColumn.id);
         columnArray.push(ColumnIDControl);
         columnArray.forEach(function(columnArrayItems) {
-            console.log(columnArrayItems);
-            return columnArrayItems;
+        console.log(columnArrayItems);
+        return columnArrayItems;
         });
             
         columnArrayItems = columnArray[0];
@@ -269,9 +296,8 @@ document.querySelector('#createTable').addEventListener('click', function() {
             rowCounter++;
             };
         // SÜTUN BAŞLIKLARI İÇİN DÖNGÜ BİTİŞ.
-        columnCounter++;
         };
-
+        columnCounter++;
         let tableEdit = document.createElement('div');
         rowAdress.appendChild(tableEdit);
         tableEdit.className = 'tableEdit';
@@ -280,13 +306,13 @@ document.querySelector('#createTable').addEventListener('click', function() {
         let tableEditButton = document.createElement('button');
         tableEdit.appendChild(tableEditButton);
         tableEditButton.className = 'edit';
-        tableEditButton.id = `edit${columnCounter}`;
+        tableEditButton.id = `rowEdit${columnCounter}`;
         tableEditButton.innerHTML = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 117.74 122.88" style="enable-background:new 0 0 117.74 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M94.62,2c-1.46-1.36-3.14-2.09-5.02-1.99c-1.88,0-3.56,0.73-4.92,2.2L73.59,13.72l31.07,30.03l11.19-11.72 c1.36-1.36,1.88-3.14,1.88-5.02s-0.73-3.66-2.09-4.92L94.62,2L94.62,2L94.62,2z M41.44,109.58c-4.08,1.36-8.26,2.62-12.35,3.98 c-4.08,1.36-8.16,2.72-12.35,4.08c-9.73,3.14-15.07,4.92-16.22,5.23c-1.15,0.31-0.42-4.18,1.99-13.6l7.74-29.61l0.64-0.66 l30.56,30.56L41.44,109.58L41.44,109.58L41.44,109.58z M22.2,67.25l42.99-44.82l31.07,29.92L52.75,97.8L22.2,67.25L22.2,67.25z"/></g></svg>';
 
         let tableDeleteButton = document.createElement('button');
         tableEdit.appendChild(tableDeleteButton);
         tableDeleteButton.className = 'delete';
-        tableDeleteButton.id = `delete${columnCounter}`;
+        tableDeleteButton.id = `rowDelete${columnCounter}`;
         tableDeleteButton.innerHTML = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="109.484px" height="122.88px" viewBox="0 0 109.484 122.88" enable-background="new 0 0 109.484 122.88" xml:space="preserve"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M2.347,9.633h38.297V3.76c0-2.068,1.689-3.76,3.76-3.76h21.144 c2.07,0,3.76,1.691,3.76,3.76v5.874h37.83c1.293,0,2.347,1.057,2.347,2.349v11.514H0V11.982C0,10.69,1.055,9.633,2.347,9.633 L2.347,9.633z M8.69,29.605h92.921c1.937,0,3.696,1.599,3.521,3.524l-7.864,86.229c-0.174,1.926-1.59,3.521-3.523,3.521h-77.3 c-1.934,0-3.352-1.592-3.524-3.521L5.166,33.129C4.994,31.197,6.751,29.605,8.69,29.605L8.69,29.605z M69.077,42.998h9.866v65.314 h-9.866V42.998L69.077,42.998z M30.072,42.998h9.867v65.314h-9.867V42.998L30.072,42.998z M49.572,42.998h9.869v65.314h-9.869 V42.998L49.572,42.998z"/></g></svg>';
 
 
